f3fbbf71a350a22e35e80c6ed0ceee312e95e5c9
kmarassm <kmdhkalite@gmail.com>
2025-12-06 23:04:24 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,22 @@
-self.addEventListener('install',e=>{self.skipWaiting()});self.addEventListener('fetch',()=>{});
+const CACHE_NAME = "depo-cache-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
